63d11d1f866eba2c7bed28719801a65b431c5f7f
Adam <adam.s.vandyke@gmail.com>
2018-04-27 15:10:26 -0400

    Json File updated simple char

--- JSON Manager/Assets/Scripts/Editor.cs
+++ JSON Manager/Assets/Scripts/Editor.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Editor : MonoBehaviour {
+    string path;
+    string jsonString;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        path = Application.streamingAssetsPath + "/json1.json";
+        jsonString = File.ReadAllText(path);
+        Map New = JsonUtility.FromJson<Map>(jsonString);
+        Debug.Log(New.Name);
+        New.Level = 25;
+        string newNew = JsonUtility.ToJson(New);
+        Debug.Log(newNew);
+    }
+}
+
+public class Map
+{
+    public string Name;
+    public int Level;
+    public int[] Stats;
 }
