1e9076435b9d819955934bdb1cb3d522b82c15ac
Adam <adam.s.vandyke@gmail.com>
2018-04-27 18:18:35 -0400

    Update
    -Editor updated.
    -Item array.
    -Sceneload.json added

--- JSON Manager/Assets/Scripts/Editor.cs
+++ JSON Manager/Assets/Scripts/Editor.cs
@@ -1,27 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using System.IO;
 
-public class Editor : MonoBehaviour {
-    string path;
-    string jsonString;
-
-    void Start()
-    {
-        path = Application.streamingAssetsPath + "/json1.json";
-        jsonString = File.ReadAllText(path);
-        Map New = JsonUtility.FromJson<Map>(jsonString);
-        Debug.Log(New.Name);
-        New.Level = 25;
-        string newNew = JsonUtility.ToJson(New);
-        Debug.Log(newNew);
-    }
-}
-
-public class Map
+public class Editor : EditorWindow
 {
-    public string Name;
-    public int Level;
-    public int[] Stats;
+    // Use this for initialization
+    [MenuItem("Import/Import Scene")]
+    static void Init()
+    {
+        ArrayScript ArrayScript;
+        string path = EditorUtility.OpenFilePanel("Overwrite with Json", Application.dataPath, "");
+
+        if (File.Exists(path))
+        {
+            string jsonstring = File.ReadAllText(path);
+            //sceneItemArray 
+            Debug.Log(jsonstring);
+            ArrayScript = JsonUtility.FromJson<ArrayScript>(jsonstring);
+
+            foreach (var sceneItem in ArrayScript.Item)
+            {
+
+                GameObject gameObject = new GameObject(sceneItem.itemType);
+                gameObject.transform.position = new Vector3(sceneItem.VecLocx, sceneItem.VecLocy, sceneItem.VecLocz);
+                gameObject.transform.rotation = Quaternion.Euler(sceneItem.VecRotX, sceneItem.VecRotY, sceneItem.VecRotZ);
+                gameObject.transform.localScale = new Vector3(sceneItem.VecSclX, sceneItem.VecSclY, sceneItem.VecSclZ);
+                if (sceneItem.Rigidbody == true)
+                {
+                    gameObject.AddComponent<Rigidbody>();
+                }
+                if (sceneItem.MeshFilter == true)
+                {
+                    gameObject.AddComponent<MeshFilter>();
+                    Debug.Log("AX");
+                    Vector3[] vertices = new Vector3[sceneItem.numVerts];
+
+                    int z = 0;
+                    for (int x = 0; x < 8; x++)
+                    {
+                        for (int y = 0; y < 3; y++)
+                        {
+                            if (y == 0)
+                            { vertices[x].x = sceneItem.Verts[z]; }
+                            if (y == 1)
+                            { vertices[x].y = sceneItem.Verts[z]; }
+                            if (y == 2)
+                            { vertices[x].z = sceneItem.Verts[z]; }
+                            Debug.Log(sceneItem.Verts[z] + " " + z);
+                            z++;
+                        }
+                    }
+                    gameObject.GetComponent<MeshFilter>().mesh.Clear();
+                    gameObject.GetComponent<MeshFilter>().mesh.vertices = vertices;
+                    gameObject.GetComponent<MeshFilter>().mesh.triangles = sceneItem.triangles;
+                    gameObject.GetComponent<MeshFilter>().mesh.RecalculateNormals();
+                }
+                if (sceneItem.BoxCollider == true)
+                {
+                    gameObject.AddComponent<BoxCollider>();
+                }
+                if (sceneItem.MeshRenderer == true)
+                {
+                    gameObject.AddComponent<MeshRenderer>();
+                    gameObject.GetComponent<MeshRenderer>().material = AssetDatabase.GetBuiltinExtraResource<Material>("Default-Diffuse.mat");
+                }
+
+                gameObject.GetComponent<MeshRenderer>().material = AssetDatabase.GetBuiltinExtraResource<Material>("Default-Diffuse.mat");
+            }
+        }
+    }
 }
---
+++ JSON Manager/Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class Item
+{
+    public string itemType;
+    public float VecLocx;
+    public float VecLocy;
+    public float VecLocz;//location to place the prefab.
+    public Vector3 Location;
+    public Vector3 Rotation; //the rotation in worldspace.
+    public Vector3 Scale; //scale of the item. 
+    public bool Rigidbody;
+    public bool MeshFilter;
+    public int[] triangles;
+    public int numVerts;
+    public int[] Verts;
+    public bool BoxCollider;
+    public bool MeshRenderer;
+    public float VecRotX;
+    public float VecRotY;
+    public float VecRotZ;
+    public float VecSclX;
+    public float VecSclY;
+    public float VecSclZ;
+}
